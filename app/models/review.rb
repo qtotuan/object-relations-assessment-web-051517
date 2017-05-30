b31318b1645e279db2afbd9b799f0a1025de918e
qtotuan <quynh.totuan@gmail.com>
2017-05-30 10:37:30 -0400

    Done.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,28 @@
 class Review
-  
-end
 
+  attr_accessor :customer, :restaurant
+
+  @@all = []
+
+  def initialize(restaurant, content)
+    @restaurant = restaurant
+    @customer
+    @content = content
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def customer
+    @customer
+  end
+
+  def restaurant
+    @restaurant
+  end
+
+  
+
+end
